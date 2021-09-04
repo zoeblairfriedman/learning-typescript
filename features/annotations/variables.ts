2ca459b61284957b14ba8d46041bfd716094336a
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-04 11:30:40 -0400

    adds features and annotations folders

---
+++ features/annotations/variables.ts
@@ -0,0 +1,5 @@
+//type annotation
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
---
+++ features/types.ts
@@ -0,0 +1,15 @@
+//types 
+
+const today = new Date();
+today.getMonth();
+
+const person = {
+  age: 20
+}
+
+class Color {
+
+}
+const red = new Color();
+
+
