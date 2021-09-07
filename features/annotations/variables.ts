a7d0719013bc1f6729ef85814aa875ed86d3bd35
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-07 16:31:16 -0400

    adds more variables

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -3,3 +3,24 @@ let apples: number = 5;
 let speed: string = 'fast';
 let hasName: boolean = true;
 let nothingMuch: null = null;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 4];
+let truths: boolean[] = [true, true, false, true];
+
+// Classes
+
+class Car {
+
+}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+}
