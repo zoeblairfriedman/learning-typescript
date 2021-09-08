6a037e9c315e8d57ec3e168015b14bb243acde1e
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-08 15:16:08 -0400

    starts annotating functions

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -36,4 +36,23 @@ const json = '{"x": 10, "y": 20}'
 const coordinates:  { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
 
-coordinates
+// 2) When we declare a variable on one line and initialize it later
+let words = [];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++){
+  if (words[i] === 'green'){
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12]; 
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++){
+  if (numbers[i] > 0){
+    numberAboveZero = numbers[i]
+  }
+}
+
