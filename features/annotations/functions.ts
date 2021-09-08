fc5ea08988dc19768ac584bed0b4a7b54b3009d8
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-08 15:26:13 -0400

    adds void and never examples

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,29 @@
+// arguments can't use inference, must use annotation
+// output can use inference, but we shouldn't use it
+
 const add = (a: number, b: number):number => {
  return a + b; 
+}
+const sub = (a: number, b: number):number => {
+ return a-b; 
+}
+
+// alt syntax
+
+function divide(a: number, b: number):number {
+  return a / b;
+}
+
+const multiply = function(a: number, b:number): number {
+  return a * b;
+}
+
+// void & never
+
+const logger = (message: string): void => {
+  console.log(message);
+}
+
+const throwError = (message:string): never => {
+  throw new Error(message);
 }
