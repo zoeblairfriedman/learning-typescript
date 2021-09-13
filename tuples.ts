0b03e598fab9824721a72f5cc610096f2b12ddae
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-13 16:20:25 -0400

    interface beginnings

--- tuples.ts
+++ tuples.ts
@@ -11,3 +11,9 @@ const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
 
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsePower: 400,
+  weight: 3354
+}
