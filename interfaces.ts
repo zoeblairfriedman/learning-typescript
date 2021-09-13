9e52fe3e750239e3712de39e4034f9685ae62c1f
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-13 16:26:10 -0400

    creates interface

--- interfaces.ts
+++ interfaces.ts
@@ -1,13 +1,31 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
 const oldCivic = {
   name: 'civic',
   year: 2000,
   broken: true
 }
+const olderCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: 2
+}
 
-const printVehicle = (vehicle: { name: string; year: number; broken: boolean}): void => {
+// const printVehicle = (vehicle: { name: string; year: number; broken: boolean}): void => {
+//   console.log(`Name: ${vehicle.name}`)
+//   console.log(`Year: ${vehicle.year}`)
+//   console.log(`Broken: ${vehicle.broken}`)
+// }
+
+const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`)
   console.log(`Year: ${vehicle.year}`)
   console.log(`Broken: ${vehicle.broken}`)
 }
 
 printVehicle(oldCivic)
+printVehicle(olderCivic)
