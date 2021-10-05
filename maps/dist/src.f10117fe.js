26c05f3643be0d894e5de600d661c5585edaa25c
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-10-05 17:31:20 -0400

    adds map to index.ts

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -136943,6 +136943,7 @@ var user = new User_1.User();
 var company = new Company_1.Company();
 console.log(user);
 console.log(company);
+google;
 },{"./User":"src/User.ts","./Company":"src/Company.ts"}],"../../../../.nvm/versions/node/v15.12.0/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -136971,7 +136972,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65535" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51488" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -2,7 +2,9 @@ import { User } from './User';
 import { Company } from './Company';
 /// <reference types="@types/google.maps" />
 
-const user = new User();
-const company = new Company();
-console.log(user)
-console.log(company)
+// const user = new User();
+// const company = new Company();
+// console.log(user)
+// console.log(company)
+
+new google.maps.Map(document.getElementById("map"))
